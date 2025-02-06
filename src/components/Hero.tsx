fb1e51c06b21e66b63b341791064b8e226eea995
Gichohi-Simon <njogugichohi56@gmail.com>
2025-02-06 17:13:00 +0300

    made some changes to the padding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div
-      className="relative flex h-96 bg-cover bg-center bg-no-repeat justify-center py-44 md:py-64"
+      className="relative flex h-96 bg-cover bg-center bg-no-repeat justify-center py-44 md:py-60"
       style={{ backgroundImage: "url('/warehouse.jpg')" }}
     >
       {/* Overlay with opacity */}
@@ -13,10 +13,10 @@ const Hero = () => {
         <p className=" font-[family-name:var(--font-poppins)] text-2xl md:text-4xl font-bold text-primary text-center">
           Gamify general <span className="text-secondary">supplies</span> and general services
         </p>
-        <p className="font-[family-name:var(--font-poppins)] mt-6 md:mt-10 text-primary text-sm md:text-xl text-center">
+        <p className="font-[family-name:var(--font-poppins)] mt-4 md:mt-6 text-primary text-base md:text-xl text-center">
           suppliers of materials used in handling <span className="text-secondary">warehouse</span> products
         </p>
-        <button className="font-[family-name:var(--font-poppins)] text-center mt-6 md:mt-12 px-4 md:px-8 py-2 md:py-3 rounded bg-secondary text-sm md:text-base">
+        <button className="font-[family-name:var(--font-poppins)] text-center mt-6 md:mt-6 px-4 md:px-8 py-2 md:py-3 rounded bg-secondary text-sm md:text-base">
           products
         </button>
       </div>
