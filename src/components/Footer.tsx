53df945fb5f8be1cc0b6a739fd1e3a44bab375db
Gichohi-Simon <njogugichohi56@gmail.com>
2025-02-06 22:37:03 +0300

    part 1 done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-primary font-[family-name:var(--font-poppins)] py-10 px-10">
+      <div>
+        <p className="font-bold text-2xl">Gamify General Supplies</p>
+        <p className="text-base">suppliers of materials used in warehouses</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -11,7 +11,7 @@ const Products = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-20">
           <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
             <Image
-              src="/warehouse.jpg"
+              src="/warehouse2.jpg"
               alt="strapping manilla"
               height="96"
               width="288"
@@ -46,126 +46,7 @@ const Products = () => {
 
           <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
             <Image
-              src="/warehouse.jpg"
-              alt="strapping manilla"
-              height="96"
-              width="288"
-              className="rounded-xl rounded-lg"
-            />
-            <div className="py-4 md:py-8 text-sm px-2 md:px-4">
-              <p className="font-bold">Strapping Manilla (9mm & 15mm)</p>
-              <p className="mt-2">
-                strong, durable rope or strap made from Manila hemp fibers
-              </p>
-              <p className="font-bold mt-2">KSH 3000</p>
-            </div>
-          </div>
-
-          <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
-            <Image
-              src="/warehouse.jpg"
-              alt="strapping manilla"
-              height="96"
-              width="288"
-              className="rounded-xl rounded-lg"
-            />
-            <div className="py-4 md:py-8 text-sm px-2 md:px-4">
-              <p className="font-bold">Strapping Manilla (9mm & 15mm)</p>
-              <p className="mt-2">
-                strong, durable rope or strap made from Manila hemp fibers
-              </p>
-              <p className="font-bold mt-2">KSH 3000</p>
-            </div>
-          </div>
-
-          <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
-            <Image
-              src="/warehouse.jpg"
-              alt="strapping manilla"
-              height="96"
-              width="288"
-              className="rounded-xl rounded-lg"
-            />
-            <div className="py-4 md:py-8 text-sm px-2 md:px-4">
-              <p className="font-bold">Strapping Manilla (9mm & 15mm)</p>
-              <p className="mt-2">
-                strong, durable rope or strap made from Manila hemp fibers
-              </p>
-              <p className="font-bold mt-2">KSH 3000</p>
-            </div>
-          </div>
-
-          <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
-            <Image
-              src="/warehouse.jpg"
-              alt="strapping manilla"
-              height="96"
-              width="288"
-              className="rounded-xl rounded-lg"
-            />
-            <div className="py-4 md:py-8 text-sm px-2 md:px-4">
-              <p className="font-bold">Strapping Manilla (9mm & 15mm)</p>
-              <p className="mt-2">
-                strong, durable rope or strap made from Manila hemp fibers
-              </p>
-              <p className="font-bold mt-2">KSH 3000</p>
-            </div>
-          </div>
-
-          <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
-            <Image
-              src="/warehouse.jpg"
-              alt="strapping manilla"
-              height="96"
-              width="288"
-              className="rounded-xl rounded-lg"
-            />
-            <div className="py-4 md:py-8 text-sm px-2 md:px-4">
-              <p className="font-bold">Strapping Manilla (9mm & 15mm)</p>
-              <p className="mt-2">
-                strong, durable rope or strap made from Manila hemp fibers
-              </p>
-              <p className="font-bold mt-2">KSH 3000</p>
-            </div>
-          </div>
-
-          <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
-            <Image
-              src="/warehouse.jpg"
-              alt="strapping manilla"
-              height="96"
-              width="288"
-              className="rounded-xl rounded-lg"
-            />
-            <div className="py-4 md:py-8 text-sm px-2 md:px-4">
-              <p className="font-bold">Strapping Manilla (9mm & 15mm)</p>
-              <p className="mt-2">
-                strong, durable rope or strap made from Manila hemp fibers
-              </p>
-              <p className="font-bold mt-2">KSH 3000</p>
-            </div>
-          </div>
-
-          <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
-            <Image
-              src="/warehouse.jpg"
-              alt="strapping manilla"
-              height="96"
-              width="288"
-              className="rounded-xl rounded-lg"
-            />
-            <div className="py-4 md:py-8 text-sm px-2 md:px-4">
-              <p className="font-bold">Strapping Manilla (9mm & 15mm)</p>
-              <p className="mt-2">
-                strong, durable rope or strap made from Manila hemp fibers
-              </p>
-              <p className="font-bold mt-2">KSH 3000</p>
-            </div>
-          </div>
-
-          <div className="w-44 md:w-72 flex flex-col items-center rounded-xl">
-            <Image
-              src="/warehouse.jpg"
+              src="/van1.jpg"
               alt="strapping manilla"
               height="96"
               width="288"
