93965666ac7a1e4676b817d4e38b32db2f595c06
Gichohi-Simon <njogugichohi56@gmail.com>
2025-02-06 18:20:49 +0300

    about section completed for mobile and web

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About section</div>
-  )
-}
+    <div className="font-[family-name:var(--font-poppins)] px-4 md:px-24 py-12 md:py-24 rounded-xl">
+      <h1 className="text-center text-xl md:text-3xl font-bold text-secondary">
+        Our Services
+      </h1>
 
-export default About
+      <div className="md:flex justify-center items-center mt-6 md:mt-12 w-full">
+        <div className="w-full md:w-1/2 px-8">
+          <p className="mt-4 text-center text-base md:text-lg">
+            {" "}
+            Suppliers of materials for warehouse handling.
+          </p>
+          <p className="mt-4 text-center text-base md:text-lg">
+            {" "}
+            For example: top and bottom capping, strapping manila, stretch wrapping film, stitching thread, slip sheets, dust masks, and various warehouse operations.
+          </p>
+        </div>
+
+        <div className="relative bg-primary h-52 md:h-96 mt-12 md:mt-0 w-3/4 md:w-1/2 rounded-xl mx-4">
+          <div
+            className="bg-cover bg-center bg-no-repeat h-52 md:h-96 w-full absolute top-12 md:top-20 left-12 md:left-16 rounded-xl"
+            style={{ backgroundImage: "url('/warehouse2.jpg')" }}
+          ></div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,20 +3,20 @@ import React from "react";
 const Hero = () => {
   return (
     <div
-      className="relative flex h-96 bg-cover bg-center bg-no-repeat justify-center py-44 md:py-60"
+      className="relative flex h-96 bg-cover bg-center bg-no-repeat justify-center py-44 md:py-60 font-[family-name:var(--font-poppins)]"
       style={{ backgroundImage: "url('/warehouse.jpg')" }}
     >
       {/* Overlay with opacity */}
       <div className="absolute inset-0 bg-black bg-opacity-75"></div>
 
       <div className="relative flex flex-col justify-center items-center mx-4 md:mx-0">
-        <p className=" font-[family-name:var(--font-poppins)] text-2xl md:text-4xl font-bold text-primary text-center">
+        <p className=" text-2xl md:text-4xl font-bold text-primary text-center">
           Gamify general <span className="text-secondary">supplies</span> and general services
         </p>
-        <p className="font-[family-name:var(--font-poppins)] mt-4 md:mt-6 text-primary text-base md:text-xl text-center">
+        <p className="mt-4 md:mt-6 text-primary text-base md:text-xl text-center">
           suppliers of materials used in handling <span className="text-secondary">warehouse</span> products
         </p>
-        <button className="font-[family-name:var(--font-poppins)] text-center mt-6 md:mt-6 px-4 md:px-8 py-2 md:py-3 rounded bg-secondary text-sm md:text-base">
+        <button className="text-center mt-6 md:mt-6 px-4 md:px-8 py-2 md:py-3 rounded bg-secondary text-sm md:text-base">
           products
         </button>
       </div>
