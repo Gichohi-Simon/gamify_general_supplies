bd3c079bd64b5f530d2e146f0fc8f1fc6e73cd5c
Gichohi-Simon <njogugichohi56@gmail.com>
2025-02-07 10:33:32 +0300

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,33 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-primary font-[family-name:var(--font-poppins)] py-10 px-10">
-      <div>
-        <p className="font-bold text-2xl">Gamify General Supplies</p>
-        <p className="text-base">suppliers of materials used in warehouses</p>
+      <div className="grid place-items-center grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="text-center md:text-start">
+          <p className="font-bold text-2xl md:text-4xl ">
+            Gamify General Supplies
+          </p>
+          <p className="text-sm md:text-base mt-2">
+            suppliers of materials used in warehouses
+          </p>
+        </div>
+        <div className="text-center md:text-start">
+          <p className="font-bold capitalize text-sm md:text-base">openning hours</p>
+          <p className="mt-1 md:mt-2 mb-1 md:mb-2 text-sm md:text-base">Mon-Fri 8:00AM to 5:00PM</p>
+          <a href="tel:+254 727 386 878" className="text-sm md:text-base">+254 727 386 878</a>
+        </div>
+        <div className="text-center md:text-start">
+          <p className="font-bold capitalize text-sm md:text-base">location</p>
+          <p className="mt-1 md:mt-2 text-sm md:text-base">
+            Mombasa, Creek View Ganjoni
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-10 md:mt-20">
+        <p className="text-center font-bold text-xs md:text-sm">
+          &copy; copyright Gamify General Supplies and general services 2025,
+          All rights reserved
+        </p>
       </div>
     </div>
   );
