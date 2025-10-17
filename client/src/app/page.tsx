83b546b985b7e30d51a75e1fff5323943db1e64d
Gichohi-Simon <njogugichohi56@gmail.com>
2025-10-17 19:01:42 +0300

    removed last remaining local api link

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,8 @@ import Hero from "@/components/Hero";
 import Products from "@/components/Products";
 
 export default async function Home() {
-  const response = await fetch("http://localhost:8080/product/getFirstThree", {
+  const API = process.env.API_URL
+  const response = await fetch(`${API}/product/getFirstThree`, {
     cache: "no-store",
   });
   const productsResponse = await response.json();
