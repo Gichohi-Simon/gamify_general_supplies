80db0f688abc08d922f4bf655e6f110bdfeeb741
Gichohi-Simon <njogugichohi56@gmail.com>
2025-10-28 12:08:22 +0300

    hero section ui improved

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,34 +1,36 @@
-import { BuildingStorefrontIcon } from "@heroicons/react/16/solid";
+import { ArrowRightCircleIcon } from "@heroicons/react/16/solid";
 import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
   return (
     <div
-      className="relative flex h-[600px] bg-cover bg-center bg-no-repeat justify-center py-44 md:py-60 font-[family-name:var(--font-poppins)]"
-      style={{ backgroundImage: "url('/warehouse.jpg')" }}
+      className="relative flex h-[400px] md:h-[600px] bg-cover bg-center bg-no-repeat  pt-28 md:pt-72 font-[family-name:var(--font-poppins)] px-2 md:px-14"
+      style={{ backgroundImage: "url('/warehouse2.jpg')" }}
     >
       {/* Overlay with opacity */}
       <div className="absolute inset-0 bg-black bg-opacity-75"></div>
-
-      <div className="relative flex flex-col justify-center items-center mx-4 md:mx-0">
-        <p className=" text-3xl md:text-5xl font-bold text-primary text-center tracking-wider">
-          Gamify general <span className="text-secondary">supplies</span> and
-          general services
+      <div className="relative text-start mx-4 md:mx-0">
+        <p className=" text-3xl md:text-5xl font-bold text-primary tracking-wider">
+          Gamify general <span className="text-secondary">supplies</span> 
         </p>
-        <p className="mt-8 md:mt-10 text-primary text-base md:text-xl text-center">
+        <p className="mt-2 text-3xl md:text-5xl font-bold text-primary tracking-wider">
+          and general services
+        </p>
+        <p className="mt-4 md:mt-4 text-primary text-base md:text-xl">
           suppliers of materials used in handling{" "}
           <span className="text-secondary">warehouse</span> products
         </p>
         <Link
           href="/shop"
-          className="mt-8 md:mt-10 px-4 md:px-6 py-2 md:py-2 rounded bg-secondary flex gap-2 items-center justify-center text-xs md:text-sm"
+          className="mt-6 md:mt-10 px-4 md:px-6 py-2 md:py-2 rounded-full bg-secondary flex justify-center items-center gap-2 w-32"
         >
+          <p className="text-sm lowercase">Shop</p>
           <span>
-            <BuildingStorefrontIcon className="size-4 md:size-6"/>
+            <ArrowRightCircleIcon className="size-4"/>
           </span>
-          Shop
         </Link>
+
       </div>
     </div>
   );
