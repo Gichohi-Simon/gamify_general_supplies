63c109569b8334014fef5141812131a91dd22598
Gichohi-Simon <njogugichohi56@gmail.com>
2025-10-28 19:41:00 +0300

    footer refurbish done

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,28 +2,51 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-primary font-[family-name:var(--font-poppins)] py-10 px-10">
-      <div className="grid place-items-center grid-cols-1 md:grid-cols-3 gap-6">
+    <div className="bg-primary font-[family-name:var(--font-poppins)] py-10 px-8 md:px-32">
+      <div className="grid grid-cols-1 md:grid-cols-4 mt-10 gap-5 md:gap-6">
         <div className="text-center md:text-start">
-          <p className="font-bold text-xl md:text-2xl ">
-            Gamify General Supplies
-          </p>
-          <p className="text-sm mt-2">
-            suppliers of materials used in warehouses
-          </p>
+          <div>
+            <p className="font-bold text-base md:text-lg uppercase">
+              Gamify General Supplies
+            </p>
+          </div>
+          <div>
+            <p className="text-xs md:text-sm mt-2">
+              suppliers of materials used in handling <br /> warehouse products
+            </p>
+          </div>
         </div>
-        <div className="text-center md:text-start">
-          <p className="font-bold capitalize text-sm md:text-base">openning hours</p>
-          <p className="mt-1 md:mt-2 mb-1 md:mb-2 text-sm">Mon-Fri 8:00AM to 5:00PM</p>
-          <a href="tel:+254 727 386 878" className="text-sm hover:font-bold">+254 727 386 878</a>
-          <br/>
-          <a href="mailto:gamify100@gmail.com" className="mt-4 hover:font-bold text-sm">gamify100@gmail.com</a>
-        </div>
-        <div className="text-center md:text-start">
+
+        <div className="text-center">
           <p className="font-bold capitalize text-sm md:text-base">location</p>
           <p className="mt-1 md:mt-2 text-sm">
             Archbishop Makarios Road, Ganjoni
           </p>
+        </div>
+
+        <div className="text-center">
+          <p className="font-bold capitalize text-xs md:text-sm">contact</p>
+          <a
+            href="tel:+254 727 386 878"
+            className="text-xs md:text-sm hover:font-bold"
+          >
+            +254 727 386 878
+          </a>
+        </div>
+
+        <div className="text-center">
+          <p className="font-bold capitalize text-sm md:text-base">
+            openning hours
+          </p>
+          <p className="mt-1 md:mt-2 mb-1 md:mb-2 text-xs md:text-sm">
+            Mon-Fri 8:00AM to 5:00PM
+          </p>
+          <a
+            href="mailto:gamify100@gmail.com"
+            className="text-xs md:text-sm hover:font-bold"
+          >
+            gamify100@gmail.com
+          </a>
         </div>
       </div>
 
