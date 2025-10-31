143bc4d7b56f234b80432f7241362d15f1d9df0c
Gichohi-Simon <njogugichohi56@gmail.com>
2025-10-31 20:44:52 +0300

    included credentials in signIn

--- client/src/api/auth.api.ts
+++ client/src/api/auth.api.ts
@@ -5,6 +5,7 @@ const API = process.env.NEXT_PUBLIC_API_URL;
 export const signIn = async (values:loginInitialValues) => {
     const response = await fetch(`${API}/auth/login`,{
         method:"POST",
+        credentials:"include",
         body:JSON.stringify(values),
         headers:{"Content-Type":"application/json"},
     });
