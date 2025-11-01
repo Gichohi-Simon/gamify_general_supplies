b7d98b011bc6219d371354df9a52ba365e8eec00
Gichohi-Simon <njogugichohi56@gmail.com>
2025-11-01 15:43:16 +0300

    removed nunito sans

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Nunito_Sans, Raleway } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 
 import Providers from "./providers";
