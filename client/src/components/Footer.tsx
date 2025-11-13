672eb84f7587f50451eb8b9516e0bebf01a27058
Gichohi-Simon <njogugichohi56@gmail.com>
2025-11-13 13:06:53 +0300

    footer fixed

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,30 +2,30 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-primary font-[family-name:var(--font-poppins)] py-10 px-8 md:px-32">
-      <div className="grid grid-cols-1 md:grid-cols-4 mt-10 gap-5 md:gap-6">
+    <div className="bg-primary font-[family-name:var(--font-poppins)] py-10 md:py-14 px-4 md:px-20">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5 md:gap-6">
         <div className="text-center md:text-start">
           <div>
-            <p className="font-bold text-base md:text-lg uppercase">
+            <p className="font-bold text-sm md:text-base capitalize tracking-wider">
               Gamify General Supplies
             </p>
           </div>
           <div>
-            <p className="text-xs md:text-sm mt-2">
+            <p className="text-xs md:text-sm capitalize mt-1 md:mt-2 tracking-wider">
               suppliers of materials used in handling <br /> warehouse products
             </p>
           </div>
         </div>
 
         <div className="text-center">
-          <p className="font-bold capitalize text-sm md:text-base">location</p>
-          <p className="mt-1 md:mt-2 text-sm">
+          <p className="font-bold capitalize text-sm md:text-base tracking-wider">location</p>
+          <p className="text-xs md:text-sm capitalize tracking-wider">
             Archbishop Makarios Road, Ganjoni
           </p>
         </div>
 
         <div className="text-center">
-          <p className="font-bold capitalize text-xs md:text-sm">contact</p>
+          <p className="font-bold capitalize text-sm md:text-base tracking-wider">contact</p>
           <a
             href="tel:+254 727 386 878"
             className="text-xs md:text-sm hover:font-bold"
@@ -35,10 +35,10 @@ const Footer = () => {
         </div>
 
         <div className="text-center">
-          <p className="font-bold capitalize text-sm md:text-base">
+          <p className="font-bold capitalize text-sm md:text-base tracking-wider">
             openning hours
           </p>
-          <p className="mt-1 md:mt-2 mb-1 md:mb-2 text-xs md:text-sm">
+          <p className="text-xs md:text-sm capitalize">
             Mon-Fri 8:00AM to 5:00PM
           </p>
           <a
@@ -50,8 +50,8 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="mt-10 md:mt-20">
-        <p className="text-center font-bold text-xs md:text-sm">
+      <div className="mt-10">
+        <p className="text-center font-bold text-xs md:text-sm tracking-wider">
           &copy; copyright Gamify General Supplies and general services 2025,
           All rights reserved
         </p>
