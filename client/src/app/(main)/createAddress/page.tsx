d4e16aa48dfb311ff64cf3e464a5242618f36b4f
Gichohi-Simon <njogugichohi56@gmail.com>
2025-11-16 16:26:35 +0300

    create address page ui changed

--- client/src/app/(main)/createAddress/page.tsx
+++ client/src/app/(main)/createAddress/page.tsx
@@ -45,115 +45,106 @@ export default function CreateAddress() {
     <div className="font-[family-name:var(--font-poppins)] flex justify-center mt-8 px-4 mb-14 md:mb-20">
       <form
         onSubmit={formik.handleSubmit}
-        className="w-full md:w-3/4 lg:w-2/3 bg-gray-100 p-6 md:p-10 rounded-xl shadow-sm space-y-5"
+        className="w-full md:w-3/4 lg:w-2/3 border border-1 p-6 md:px-6 md:py-8 rounded-xl shadow-sm space-y-5"
       >
-        <p className="text-lg font-semibold tracking-wider capitalize">
-          Update Address
-        </p>
         <div>
-          <label
-            htmlFor="companyName"
-            className="font-semibold text-xs md:text-sm tracking-wider"
-          >
-            Company Name
-          </label>
-          <input
-            type="text"
-            placeholder="Company name"
-            name="companyName"
-            className="border border-gray-300 tracking-wider py-2 px-3 text-xs md:text-sm w-full mt-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.companyName}
-          />
-          {formik.touched.companyName && formik.errors.companyName && (
-            <p className="text-red-500 mt-1 font-medium text-xs">
-              {formik.errors.companyName}
-            </p>
-          )}
+          <p className="text-sm font-bold tracking-wider capitalize">Create Address</p>
+          <p className="text-[10px] md:text-xs">manage your address details here</p>
         </div>
 
-        <div>
-          <label
-            htmlFor="street"
-            className="font-semibold text-xs md:text-sm tracking-wider"
-          >
-            Street
-          </label>
-          <input
-            type="text"
-            placeholder="Street name"
-            name="street"
-            className="tracking-wider border border-gray-300 py-2 px-3 text-xs md:text-sm w-full mt-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.street}
-          />
-          {formik.touched.street && formik.errors.street && (
-            <p className="text-red-500 mt-1 font-medium text-xs">
-              {formik.errors.street}
-            </p>
-          )}
+        <div className="flex gap-4">
+          <div className="w-1/2">
+            <label htmlFor="companyName" className="text-[10px] md:text-xs tracking-wider">
+              Company Name
+            </label>
+            <input
+              type="text"
+              placeholder="Company name"
+              name="companyName"
+              className="border border-gray-300 tracking-wider py-2 px-3 text-[10px] md:text-xs w-full mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.companyName}
+            />
+            {formik.touched.companyName && formik.errors.companyName && (
+              <p className="text-red-500 mt-1 font-medium text-xs">
+                {formik.errors.companyName}
+              </p>
+            )}
+          </div>
+
+          <div className="w-1/2">
+            <label htmlFor="street" className="text-[10px] md:text-xs tracking-wider">
+              Street
+            </label>
+            <input
+              type="text"
+              placeholder="Street name"
+              name="street"
+              className="tracking-wider border border-gray-300 py-2 px-3 text-[10px] md:text-xs w-full mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.street}
+            />
+            {formik.touched.street && formik.errors.street && (
+              <p className="text-red-500 mt-1 font-medium text-xs">
+                {formik.errors.street}
+              </p>
+            )}
+          </div>
         </div>
 
-        <div>
-          <label
-            htmlFor="floorNumber"
-            className="font-semibold text-xs md:text-sm tracking-wider"
-          >
-            Floor Number
-          </label>
-          <input
-            type="text"
-            placeholder="Floor number"
-            name="floorNumber"
-            className="tracking-wider border border-gray-300 py-2 px-3 text-xs md:text-sm w-full mt-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.floorNumber}
-          />
-          {formik.touched.floorNumber && formik.errors.floorNumber && (
-            <p className="text-red-500 mt-1 font-medium text-xs">
-              {formik.errors.floorNumber}
-            </p>
-          )}
+        <div className="flex gap-4">
+          <div className="w-1/2">
+            <label htmlFor="floorNumber" className="text-[10px] md:text-xs tracking-wider">
+              Floor Number
+            </label>
+            <input
+              type="text"
+              placeholder="Floor number"
+              name="floorNumber"
+              className="tracking-wider border border-gray-300 py-2 px-3 text-[10px] md:text-xs w-full mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.floorNumber}
+            />
+            {formik.touched.floorNumber && formik.errors.floorNumber && (
+              <p className="text-red-500 mt-1 font-medium text-xs">
+                {formik.errors.floorNumber}
+              </p>
+            )}
+          </div>
+
+          <div className="w-1/2">
+            <label htmlFor="city" className="text-[10px] md:text-xs tracking-wider">
+              City
+            </label>
+            <input
+              type="text"
+              placeholder="City"
+              name="city"
+              className="tracking-wider border border-gray-300 py-2 px-3 text-[10px] md:text-xs w-full mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.city}
+            />
+            {formik.touched.city && formik.errors.city && (
+              <p className="text-red-500 mt-1 font-medium text-xs">
+                {formik.errors.city}
+              </p>
+            )}
+          </div>
         </div>
 
-        <div>
-          <label
-            htmlFor="city"
-            className="font-semibold text-xs md:text-sm tracking-wider"
-          >
-            City
-          </label>
-          <input
-            type="text"
-            placeholder="City"
-            name="city"
-            className="tracking-wider border border-gray-300 py-2 px-3 text-xs md:text-sm w-full mt-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.city}
-          />
-          {formik.touched.city && formik.errors.city && (
-            <p className="text-red-500 mt-1 font-medium text-xs">
-              {formik.errors.city}
-            </p>
-          )}
-        </div>
-
-        <div>
-          <label
-            htmlFor="postalCode"
-            className="font-semibold text-xs md:text-sm tracking-wider"
-          >
+        <div className="w-1/2">
+          <label htmlFor="postalCode" className="text-[10px] md:text-xs tracking-wider">
             Postal Code
           </label>
           <input
             type="text"
             placeholder="Postal code"
             name="postalCode"
-            className="tracking-wider border border-gray-300 py-2 px-3 text-xs md:text-sm w-full mt-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="tracking-wider border border-gray-300 py-2 px-3 text-[10px] md:text-xs w-full mt-1 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.postalCode}
@@ -165,10 +156,10 @@ export default function CreateAddress() {
           )}
         </div>
 
-        <div className="pt-4 flex gap-4">
+        <div className="pt-4 flex justify-end gap-4">
           <button
             type="submit"
-            className="tracking-wider bg-primary text-xs font-semibold py-2 px-6 rounded-md transition duration-200"
+            className="tracking-wider bg-secondary hover:bg-primary text-[10px] md:text-xs py-2 px-6 rounded-full transition duration-200"
           >
             Create Address
           </button>
