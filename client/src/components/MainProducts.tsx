d3fd8c8418c0b797c82874bc0262577e4bb125c9
Gichohi-Simon <njogugichohi56@gmail.com>
2025-11-17 15:21:25 +0300

    shop page grid

--- client/src/components/MainProducts.tsx
+++ client/src/components/MainProducts.tsx
@@ -13,8 +13,8 @@ const MainProducts = () => {
       >
         <div className="absolute inset-0 bg-black bg-opacity-65"></div>
 
-        <div className="relative z-10 text-start text-white">
-          <p className="my-3 mx-10 md:my-6 text-2xl md:text-4xl text-primary font-bold tracking-wider">
+        <div className="relative z-10 text-start text-white mx-[30px] md:mx-[60px]">
+          <p className="my-3 md:my-6 text-2xl md:text-4xl text-primary font-bold tracking-wider">
             Get the right tools,{" "}
             <span className="text-secondary">equipment</span>, and{" "}
             <span className="text-secondary">supplies </span>
@@ -22,13 +22,13 @@ const MainProducts = () => {
             <span className="text-secondary">warehouse </span>
             running smoothly.
           </p>
-          <p className="text-sm md:text-base mx-10 text-secondary tracking-wider">
+          <p className="text-sm md:text-base text-secondary tracking-wider">
             Quality you can count on, delivered when you need it.
           </p>
         </div>
       </div>
 
-      <div className="w-full px-8 md:px-20 md:flex justify-between items-center mt-6">
+      <div className="w-full px-8 md:px-[60px] md:flex justify-between items-center mt-6">
         <Search />
         <Categories />
       </div>
--- client/src/components/ShopProductsComponent.tsx
+++ client/src/components/ShopProductsComponent.tsx
@@ -12,8 +12,8 @@ interface AllPosts {
 export default function ShopProductsComponent({ products }: AllPosts) {
   return (
     <div className="mt-8 md:mt-10">
-      <div className="flex justify-center px-6 md:px-0">
-        <div className="w-full max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-10 pb-20 md:pb-20 pt-5 md:pt-10">
+      <div className="flex justify-center">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-10 pb-20 md:pb-20 pt-5 md:pt-10 mx-[30px] md:mx-[60px]">
           {products?.map((catalog) => (
             <div key={catalog.name} className="flex flex-col items-center border hover:border-primary rounded-lg px-4 py-3 shadow-md">
               <span className="bg-primary py-1 px-2 text-[10px] md:text-xs capitalize self-start rounded">
