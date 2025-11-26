32952e7f1d5a7c19176829a3f157b28928d76ddb
Gichohi-Simon <njogugichohi56@gmail.com>
2025-11-26 18:54:45 +0300

    footer done

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,66 +1,94 @@
 import React from "react";
+import { ArrowRightCircleIcon } from "@heroicons/react/16/solid";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="bg-primary font-[family-name:var(--font-poppins)] py-10 px-4 md:px-[60px]">
-       <div>
-        <p className="text-center font-bold text-xs md:text-sm tracking-wider py-6">
-          &copy; copyright Gamify General Supplies and general services 2025,
-          All rights reserved
-        </p>
-      </div>
-      <div className="flex justify-between">
+    <div className="bg-primary font-[family-name:var(--font-poppins)] py-20 px-4 md:px-[60px]">
+      <div className="text-center md:text-start md:flex justify-between">
         <div>
-          <div className="">
+          <div>
             <div>
               <p className="font-bold text-sm md:text-base capitalize tracking-wider">
                 Gamify General Supplies
               </p>
             </div>
             <div>
-              <p className="text-xs md:text-sm capitalize mt-1 md:mt-2 tracking-wider">
+              <p className="text-[10px] md:text-xs capitalize mt-1 md:mt-2 tracking-wider">
                 suppliers of materials used in handling <br /> warehouse
                 products
               </p>
             </div>
           </div>
+          <div className="flex justify-center md:justify-start">
+            <Link href="/shop">
+              <span className="bg-black text-white text-[10px] md:text-xs px-4 py-2 rounded mt-6 tracking-wider capitalize flex gap-2">
+                start shopping
+                <ArrowRightCircleIcon className="size-4" />
+              </span>
+            </Link>
+          </div>
+        </div>
 
-          <div className="mt-4">
-            <p className="font-bold capitalize text-sm md:text-base tracking-wider">
+        <div className="text-center md:text-start mt-8 md:mt-0">
+          <div className="mt-2">
+            <p className="font-bold capitalize text-xs md:text-sm tracking-wider">
               location
             </p>
-            <p className="text-xs md:text-sm capitalize tracking-wider">
-              Archbishop Makarios Road, Ganjoni
+            <p className="text-[10px] md:text-xs capitalize tracking-wider">
+              NSSF Building, 7th Floor
             </p>
           </div>
 
-          <div className="mt-4">
-            <p className="font-bold capitalize text-sm md:text-base tracking-wider">
+          <div className="mt-3">
+            <p className="font-bold capitalize text-xs md:text-sm tracking-wider">
+              warehouse location
+            </p>
+            <p className="text-[10px] md:text-xs capitalize tracking-wider">
+              Shimanzi jua kali
+            </p>
+          </div>
+
+          <div className="mt-3">
+            <p className="font-bold capitalize text-xs md:text-sm tracking-wider">
               contact
             </p>
             <a
               href="tel:+254 727 386 878"
-              className="text-xs md:text-sm hover:font-bold"
+              className="text-[10px] md:text-xs hover:font-bold"
             >
               +254 727 386 878
             </a>
           </div>
         </div>
 
-        <div className="text-center">
-          <p className="font-bold capitalize text-sm md:text-base tracking-wider">
+        <div className="mt-8 md:mt-0">
+          <p className="font-bold capitalize text-xs md:text-sm tracking-wider">
             openning hours
           </p>
-          <p className="text-xs md:text-sm capitalize">
+          <p className="text-[10px] md:text-xs capitalize">
             Mon-Fri 8:00AM to 5:00PM
           </p>
           <a
             href="mailto:gamify100@gmail.com"
-            className="text-xs md:text-sm hover:font-bold"
+            className="text-[10px] md:text-xs hover:font-bold"
           >
             gamify100@gmail.com
           </a>
         </div>
+      </div>
+      <hr className="my-8 border-1 border-black opacity-10" />
+      <div className="text-center md:flex justify-between mt-6">
+        <p className="font-bold text-xs md:text-sm tracking-wider">
+          &copy; 2025 Gamify General Supplies and general services, All rights
+          reserved.
+        </p>
+        <p className="text-xs md:text-sm">
+          Developed by{" "}
+          <a href="https://gichohi.com">
+            <span className="font-bold">gichohi</span>
+          </a>
+        </p>
       </div>
     </div>
   );
