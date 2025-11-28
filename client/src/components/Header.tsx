907ac7cfb5dd70385ec16f89bfb6f019e113834a
Gichohi-Simon <njogugichohi56@gmail.com>
2025-11-28 23:21:33 +0300

    header fixed

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
   const cartCount = isMounted ? cart.length : 0;
 
   return (
-    <Popover className="flex justify-between items-center px-2 md:px-5 py-4 mx-[20px] md:mx-[40px] font-[family-name:var(--font-poppins)]">
+    <Popover className="flex justify-between items-center px-2 md:px-5 py-4 mx-[20px] md:mx-[40px] font-[family-name:var(--font-poppins)] relative z-50">
       <div className="text-xs md:text-sm">
         <Link href="/" className="flex flex-col">
           <span className="font-bold text-base md:text-xl uppercase tracking-wider">
@@ -40,6 +40,7 @@ const Header = () => {
         </Link>
       </div>
 
+      {/* Desktop Menu */}
       <div className="hidden md:flex gap-4 items-center justify-between">
         <Link
           href="/shop"
@@ -72,62 +73,74 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="block md:hidden">
-        <PopoverButton>
-          <Bars3Icon className="size-6" />
-        </PopoverButton>
-      </div>
+      {/* Mobile Menu */}
+      <Popover>
+        {({ close }) => (
+          <>
+            <div className="block md:hidden">
+              <PopoverButton>
+                <Bars3Icon className="size-6" />
+              </PopoverButton>
+            </div>
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
-      >
-        <PopoverPanel className="absolute block inset-x-0 md:hidden px-2 py-5 mt-6 bg-white">
-          <PopoverButton>
-            <XCircleIcon className="size-6" />
-          </PopoverButton>
-
-          <div className="flex flex-col">
-            <Link
-              href="/shop"
-              className="text-base capitalize mt-4 hover:text-secondary"
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
             >
-              <span className="flex gap-2">
-                <BuildingStorefrontIcon className="size-6" />
-                <p className="text-sm">Store</p>
-              </span>
-            </Link>
+              <PopoverPanel className="fixed inset-0 md:hidden bg-secondary z-50 px-4 py-6">
+                <div className="flex justify-end mb-6">
+                  <PopoverButton>
+                    <XCircleIcon className="text-black size-6" />
+                  </PopoverButton>
+                </div>
 
-            <Link
-              href="/account"
-              className="capitalize mt-4 hover:text-secondary"
-            >
-              <span className="flex gap-2">
-                <UserCircleIcon className="size-6" />
-                <p className="text-sm">Account</p>
-              </span>
-            </Link>
+                <div className="flex flex-col text-white">
+                  <Link
+                    href="/shop"
+                    className="text-base capitalize mt-4 hover:text-primary"
+                    onClick={() => close()}
+                  >
+                    <span className="flex gap-2 items-center">
+                      <BuildingStorefrontIcon className="text-black size-6" />
+                      <p className="text-sm text-black">Store</p>
+                    </span>
+                  </Link>
 
-            <Link
-              href="/cart"
-              className="text-base capitalize mt-4 hover:text-secondary"
-            >
-              <span className="flex gap-2 bg-transparent relative">
-                <p className="bg-secondary absolute left-5 top-[135px] text-sm w-5 h-5 flex justify-center items-center rounded-full text-black">
-                  {cartCount}
-                </p>
-                <ShoppingCartIcon className="size-6" />
-                <p className="text-sm">Cart</p>
-              </span>
-            </Link>
-          </div>
-        </PopoverPanel>
-      </Transition>
+                  <Link
+                    href="/account"
+                    className="capitalize mt-4 hover:text-primary"
+                    onClick={() => close()}
+                  >
+                    <span className="flex gap-2 items-center">
+                      <UserCircleIcon className="text-black size-6" />
+                      <p className="text-sm text-black">Account</p>
+                    </span>
+                  </Link>
+
+                  <Link
+                    href="/cart"
+                    className="text-base capitalize mt-4 hover:text-primary"
+                    onClick={() => close()}
+                  >
+                    <span className="flex gap-2 items-center relative">
+                      <p className="bg-white text-black absolute left-5 top-[-10px] text-sm w-5 h-5 flex justify-center items-center rounded-full">
+                        {cartCount}
+                      </p>
+                      <ShoppingCartIcon className="text-black size-6" />
+                      <p className="text-sm text-black">Cart</p>
+                    </span>
+                  </Link>
+                </div>
+              </PopoverPanel>
+            </Transition>
+          </>
+        )}
+      </Popover>
     </Popover>
   );
 };
